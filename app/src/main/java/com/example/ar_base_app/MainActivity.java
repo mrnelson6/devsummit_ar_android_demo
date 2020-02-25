40f174c0aaf59d9b62666c23547ee3e9213e33b0
Matthew Nelson <m.nelson@esri.com>
2020-02-25 10:46:51 -0800

    Setup base AR app. Configures dependencies, adds the ARview, shows a globe to verify we setup the app correctly

--- app/src/main/java/com/example/ar_base_app/MainActivity.java
+++ app/src/main/java/com/example/ar_base_app/MainActivity.java
@@ -4,11 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.esri.arcgisruntime.mapping.ArcGISScene;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.toolkit.ar.ArcGISArView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArcGISArView mArView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get the AR view from the activity
+        mArView = findViewById(R.id.arView);
+        mArView.registerLifecycle(getLifecycle());
+
+        //Adding these 2 lines shows the globe
+        //This verifies we setup the app correctly
+        ArcGISScene scene = new ArcGISScene(Basemap.createImagery());
+        mArView.getSceneView().setScene(scene);
+    }
+
+    @Override
+    public synchronized void onResume() {
+        super.onResume();
+
+        //start tracking when app is being used
+        if (mArView != null) {
+            mArView.startTracking(ArcGISArView.ARLocationTrackingMode.INITIAL);
+        }
+    }
+
+    @Override
+    public synchronized void onPause() {
+        //stop tracking when app is not being used
+        if (mArView != null) {
+            mArView.stopTracking();
+        }
+        super.onPause();
     }
 }
