19cb0194e4e2ee5df7cbd5b5df63b9618b99b0ff
Matthew Nelson <m.nelson@esri.com>
2020-03-04 12:37:02 -0800

    Setup permissions

--- app/src/main/java/com/example/ar_base_app/MainActivity.java
+++ app/src/main/java/com/example/ar_base_app/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ar_base_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -42,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         mArView = findViewById(R.id.arView);
         mArView.registerLifecycle(getLifecycle());
 
+        requestPermissions();
         // on tap
         mArView.getSceneView().setOnTouchListener(new DefaultSceneViewOnTouchListener(mArView.getSceneView()) {
             @Override public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
@@ -174,5 +179,37 @@ public class MainActivity extends AppCompatActivity {
             mArView.stopTracking();
         }
         super.onPause();
+    }
+
+    /**
+     * Request read external storage for API level 23+.
+     */
+    private void requestPermissions() {
+        // define permission to request
+        String[] reqPermission = { Manifest.permission.INTERNET,
+                Manifest.permission.CAMERA};
+        int requestCode = 2;
+        if (ContextCompat.checkSelfPermission(this, reqPermission[0]) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, reqPermission[1]) == PackageManager.PERMISSION_GRANTED){
+        } else {
+            // request permission
+            ActivityCompat.requestPermissions(this, reqPermission, requestCode);
+        }
+    }
+
+    /**
+     * Handle the permissions request response.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (grantResults.length >= 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                && grantResults[1] == PackageManager.PERMISSION_GRANTED)
+        {
+        } else {
+            // report to user that permission was denied
+            Toast.makeText(this, "Permissions denied", Toast.LENGTH_SHORT).show();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
